c38a84f25a773378ac85c831b9af364607931c73
root <root@vagrant-centos65.vagrantup.com>
2016-10-23 05:12:58 +0000

    BBS simple module3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-@bbspost					= Bbspost.create
-@bbspost.title		= 'test title No.1'
-@bbspost.username	= 'Tokyo Taro'
-@bbspost.content	= 'A quick brown fox jumps over the lazy dog. '
-@bbspost.save
+# coding: windows-31j
 
 @bbspost					= Bbspost.create
-@bbspost.title		= 'test title No.2'
-@bbspost.username	= 'Okinawa Jiro'
-@bbspost.content	= 'Mr. Jock, TV Quiz Ph.D, bags few lynx.'
+@bbspost.title		= '使い方'
+@bbspost.username	= '説明する人'
+@bbspost.content	= 'POSTから入力することで、ここに新しい順に羅列されます。これは最も古い投稿です。'
 @bbspost.save
 
